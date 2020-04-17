cfbc512ece35b30098078c2c3dd7bce40ae98438
myari211 <syukrihsb148@gmail.com>
2020-04-17 08:42:16 +0700

    fix some change

--- lib/layout/main/main.dart
+++ lib/layout/main/main.dart
@@ -3,57 +3,80 @@ import 'package:absent_flutter/layout/main/widgets/actions/identity.dart';
 import 'package:absent_flutter/layout/main/widgets/bottomBar/bottomBar.dart';
 import 'package:flutter/material.dart';
 
-class LayoutMain extends StatelessWidget {
+class LayoutMain extends StatefulWidget {
   final Widget child;
   final String title;
   const LayoutMain({Key key, @required this.child, @required this.title})
       : super(key: key);
 
   @override
+  _LayoutMainState createState() => _LayoutMainState();
+}
+
+class _LayoutMainState extends State<LayoutMain> {
+  ScrollController _scrollController;
+  bool shouldChangeBackgroundColor = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _scrollController = ScrollController();
+    _scrollController.addListener(() {
+      if (_scrollController.offset >= 50) {
+        setState(() => shouldChangeBackgroundColor = true);
+      } else {
+        setState(() => shouldChangeBackgroundColor = false);
+      }
+    });
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        // appBar: AppBar(
-        //   actions: actions(),
-        //   elevation: 0,
-        // ),
         body: CustomScrollView(
+          controller: _scrollController,
           physics: const BouncingScrollPhysics(),
           slivers: <Widget>[
             SliverAppBar(
               pinned: true,
               expandedHeight: 250,
               elevation: 0,
-              flexibleSpace: FlexibleSpaceBar(
-                background: Stack(
-                  fit: StackFit.expand,
-                  children: [
-                    Container(
-                      color: Theme.of(context).canvasColor,
-                      child: Image(
-                        image: AssetImage("assets/images/download.png"),
-                        fit: BoxFit.cover,
-                      ),
+              backgroundColor: shouldChangeBackgroundColor
+                  ? Theme.of(context).appBarTheme.color
+                  : Theme.of(context).canvasColor,
+              flexibleSpace: LayoutBuilder(
+                builder: (context, constraint) {
+                  return FlexibleSpaceBar(
+                    background: Stack(
+                      fit: StackFit.expand,
+                      children: [
+                        Image(
+                          image: AssetImage("assets/images/download.png"),
+                          fit: BoxFit.cover,
+                        ),
+                        Image(
+                          image: AssetImage("assets/images/blob-shape.png"),
+                          fit: BoxFit.scaleDown,
+                          alignment: Alignment(1, -0.5),
+                        ),
+                        Image(
+                          image: AssetImage("assets/images/profile.png"),
+                          fit: BoxFit.scaleDown,
+                          alignment: Alignment(0.6, 0),
+                        ),
+                        Identity()
+                      ],
                     ),
-                    Image(
-                      image: AssetImage("assets/images/blob-shape.png"),
-                      fit: BoxFit.scaleDown,
-                      alignment: Alignment(1, -0.5),
-                    ),
-                    Image(
-                      image: AssetImage("assets/images/profile.png"),
-                      fit: BoxFit.scaleDown,
-                      alignment: Alignment(0.6, 0),
-                    ),
-                    Identity()
-                  ],
-                ),
+                  );
+                },
               ),
               actions: actions(),
             ),
             SliverList(
               delegate: SliverChildListDelegate(
                 <Widget>[
-                  this.child,
+                  this.widget.child,
                 ],
               ),
             ),
