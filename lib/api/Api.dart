1385b271bdea881415ea5605b095ac888e062043
myari211 <syukrihsb148@gmail.com>
2020-04-20 06:32:54 +0700

    :ambulance: fix fetchCourses

--- lib/api/Api.dart
+++ lib/api/Api.dart
@@ -4,23 +4,13 @@ class Api extends http.BaseClient {
   Api._privateConstructor();
 
   final String token = "null";
-  final String url = "http://localhost";
   final http.Client _inner = http.Client();
 
   static final Api instance = Api._privateConstructor();
 
   @override
   Future<http.StreamedResponse> send(http.BaseRequest request) {
-    print(request);
-    Uri uri = Uri(
-      scheme: "http",
-      port: 3030,
-      host: url,
-      path: "/course",
-    );
-
-    http.Request newRequest = http.Request(request.method, uri);
-    newRequest.headers["token"] = this.token;
-    return _inner.send(newRequest);
+    request.headers["token"] = this.token;
+    return _inner.send(request);
   }
 }
