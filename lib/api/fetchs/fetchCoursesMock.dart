924f58c3053b09d6fa05e87f9bede6fb33331351
myari211 <syukrihsb148@gmail.com>
2020-04-20 06:14:35 +0700

    :truck: renaming mock courses and course

--- lib/api/fetchs/fetchCoursesMock.dart
+++ lib/api/fetchs/fetchCoursesMock.dart
@@ -1,14 +1,14 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:absent_flutter/api/fetchs/fetchCourseMock.dart';
 import 'package:absent_flutter/api/fetchs/fetchCourses.dart';
-import 'package:absent_flutter/api/fetchs/fetchMockCourse.dart';
 import 'package:absent_flutter/entities/Course.dart';
 import 'package:flutter/foundation.dart';
 
-Future<List<Course>> fetchMockCourses() async {
+Future<List<Course>> fetchCoursesMock() async {
   sleep(Duration(milliseconds: 500));
-  final course = await fetchMockCourse();
+  final course = await fetchCourseMock();
   var mockData = json.encode(<Course>[
     course,
     course,
