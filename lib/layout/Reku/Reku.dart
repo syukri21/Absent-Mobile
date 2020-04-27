5715455df5f0d6b4c6a6280d928204adb69b7376
myari211 <syukrihsb148@gmail.com>
2020-04-27 11:58:10 +0700

    log

--- lib/layout/Reku/Reku.dart
+++ lib/layout/Reku/Reku.dart
@@ -18,6 +18,7 @@ class LayoutReku extends StatelessWidget {
             children: <Widget>[
               Icon(Icons.insert_chart),
               Expanded(
+                // dssd
                 child: Center(
                   child: Wrap(
                     children: <Widget>[
